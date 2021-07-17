a5ea31e4d033b77b6dcaf670f2c630bb7bd74501
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2021-07-17 15:59:34 +0530

    Add routes

--- src/index.js
+++ src/index.js
@@ -9,59 +9,118 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req,res) => {
+// User Routes
+
+app.post('/users', async (req,res) => {
     const user = new User(req.body)
-    user.save().then((user) => {
-        res.status(201).send(user);
-    }).catch(err => {
+    try{
+        await user.save()
+        res.status(201).send(user)
+    } catch(err) {
         res.status(400).send(err)
-    })
+    }
 })
 
-app.get('/users', (req,res) => {
-    User.find({}).then((users) => {
-        res.send(users);
-    }).catch(err => {
-        res.status(500).send(err)
-    })
+app.get('/users', async (req,res) => {
+    try{
+        const users = await User.find({})
+        res.status(201).send(users)
+    } catch(err) {
+        res.status(400).send(err)
+    }
 })
 
-app.get('/users/:id', (req,res) => {
-    const _id = req.params.id;
-    User.findById({ _id }).then((user) => {
+app.get('/users/:id', async (req,res) => {
+    try{
+        const user = await User.findById(req.params.id)
         if(!user) return res.status(404).send()
-        res.send(user);
-    }).catch(err => {
+        res.send(user)
+    } catch(err){
         res.status(500).send(err)
-    })
+    }
 })
 
-app.post('/tasks', (req,res) => {
-    const task = new Task(req.body)
-    task.save().then((task) => {
-        res.status(201).send(task);
-    }).catch(err => {
+app.patch('/users/:id', async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValid = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValid) return res.status(400).send({ error: 'Invalid updates'})
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user) return res.status(404).send()
+        res.send(user)
+    } catch(err){
         res.status(400).send(err)
-    })
+    }
 })
 
-app.get('/tasks', (req,res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks);
-    }).catch(err => {
+app.delete('/users/:id', async (req,res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) return res.status(404).send()
+        res.send(user)
+    } catch(err){
         res.status(500).send(err)
-    })
+    }
 })
 
+// Task routes
 
-app.get('/tasks/:id', (req,res) => {
-    const _id = req.params.id;
-    Task.findById({ _id }).then((task) => {
+app.post('/tasks', async (req,res) => {
+    const task = new Task(req.body)
+    try{
+        await task.save()
+        res.status(201).send(task);
+    } catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+app.get('/tasks', async (req,res) => {
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+})
+
+app.get('/tasks/:id', async (req,res) => {
+    try{
+        const task = await Task.findById(req.params.id)
         if(!task) return res.status(404).send()
         res.send(task);
-    }).catch(err => {
+    } catch(err){
         res.status(500).send(err)
-    })
+    }
+})
+
+app.patch('/tasks/:id', async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValid = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValid) return res.status(400).send({ error: 'Invalid updates'})
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!task) return res.status(404).send()
+        res.send(task)
+    } catch(err){
+        res.status(400).send(err)
+    }
+})
+
+app.delete('/tasks/:id', async (req,res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task) return res.status(404).send()
+        res.send(task)
+    } catch(err){
+        res.status(500).send(err)
+    }
 })
 
 app.listen(port, () => {
