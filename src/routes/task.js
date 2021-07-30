b6856005bb5748300831d20a91b08b035eff9722
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2021-07-30 22:13:05 +0530

    Fix delete route for tasks

--- src/routes/task.js
+++ src/routes/task.js
@@ -85,7 +85,7 @@ router.patch('/:id', auth, async (req,res) => {
 
 router.delete('/:id', auth, async (req,res) => {
     try{
-        const task = await Task.findByIdAndDelete({ _id: req.params.id, owner: req.user._id})
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id})
         if(!task) return res.status(404).send()
         res.send(task)
     } catch(err){
